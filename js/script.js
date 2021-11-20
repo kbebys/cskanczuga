b79363ecea229086861fe48246cacdae2d39645a
kbebys <kbebys@gmail.com>
2021-11-20 22:16:45 +0100

    Home Page performance update
    - change gallery carousel library
    - remove fontawesome
    - remove jquery

--- js/script.js
+++ js/script.js
@@ -58,3 +58,30 @@ const setAriaExpanded = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+new Glide('.gallery-wrapper', {
+	//desktop-first
+	type: 'carousel',
+	startAt: 0,
+	perView: 2,
+	peek: 15,
+	gap: 30,
+	autoplay: 3000,
+	breakpoints: {
+		992: {
+			perView: 1,
+			peek: 120,
+			gap: 40,
+		},
+		768: {
+			perView: 1,
+			peek: 60,
+			gap: 20,
+		},
+		576: {
+			perView: 1,
+			gap: 10,
+			peek: 30,
+		},
+	},
+}).mount()
